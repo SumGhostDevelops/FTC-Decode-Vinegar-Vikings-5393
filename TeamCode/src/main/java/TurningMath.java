df33232d98e9876d5a9cd41106297f973874a6da
ianw <ianwight123@gmail.com>
2025-10-06 16:48:54 -0500

    Hopefully finalized math to turn degrees to turn into the ammount of time to activate the motors at full speed.

--- TeamCode/src/main/java/TurningMath.java
+++ TeamCode/src/main/java/TurningMath.java
@@ -2,9 +2,9 @@ import java.lang.Math;
 public class TurningMath
 {
     double timeToTurnNinety = 0.204;
-    double Lx = 1;
-    double Ly = 1;
-    double r = 1;
+    double Lx = 0.410;
+    double Ly = 0.336;
+    double r = 0.052;
     public double Calculate (double degrees)
     {
         double radiansToTurn = (Math.sqrt(Math.pow(Lx,2)+Math.pow(Lx,2))*degrees)/r;
