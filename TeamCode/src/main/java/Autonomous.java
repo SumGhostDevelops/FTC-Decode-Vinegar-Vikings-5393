f672bde2a565331de3de30fdb1e446e0991486fb
Vivi C <69370002+SumGhostDevelops@users.noreply.github.com>
2025-10-06 17:02:38 -0500

    fized auto

--- TeamCode/src/main/java/Autonomous.java
+++ TeamCode/src/main/java/Autonomous.java
@@ -1,21 +1,22 @@
 import androidx.annotation.NonNull;
 
 import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 
 public class Autonomous { private DcMotor frontLeftMotor, backLeftMotor, frontRightMotor, backRightMotor;
     // Initialize the hardware variables. Note that the strings used here must correspond
     // to the names assigned during the robot configuration step on the DS or RC devices
         public void init(HardwareMap hardwareMap) {
-            frontLeftDrive = hardwareMap.get(DcMotor.class, "front_left_drive");
-            backLeftDrive = hardwareMap.get(DcMotor.class, "back_left_drive");
-            frontRightDrive = hardwareMap.get(DcMotor.class, "front_right_drive");
-            backRightDrive = hardwareMap.get(DcMotor.class, "back_right_drive");
-            frontLeftDrive.setDirection(DcMotor.Direction.REVERSE);
-            backLeftDrive.setDirection(DcMotor.Direction.REVERSE);
-            frontRightDrive.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
-            backRightDrive.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
-            frontLeftDrive.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
-            backLeftDrive.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+            frontLeftMotor = hardwareMap.get(DcMotor.class, "front_left_drive");
+            backLeftMotor = hardwareMap.get(DcMotor.class, "back_left_drive");
+            frontRightMotor = hardwareMap.get(DcMotor.class, "front_right_drive");
+            backRightMotor = hardwareMap.get(DcMotor.class, "back_right_drive");
+            frontLeftMotor.setDirection(DcMotor.Direction.REVERSE);
+            backLeftMotor.setDirection(DcMotor.Direction.REVERSE);
+            frontRightMotor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+            backRightMotor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+            frontLeftMotor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+            backLeftMotor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
         }
 }
