2a6058b618e0346f0be6a584c1658a0b61dd5df1
Caleb C <crd.5681@outlook.com>
2025-10-26 17:12:24 -0500

    update exception to be more general

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/exceptions/TooManyTagsDetectedException.java
@@ -0,0 +1,19 @@
+package org.firstinspires.ftc.teamcode.exceptions;
+
+public class TooManyTagsDetectedException extends Exception
+{
+    public TooManyTagsDetectedException()
+    {
+        super("Too many AprilTags were detected.");
+    }
+
+    /**
+     * "Too many AprilTags (" + actualTagCount + ") were detected. Expected " + expectedTagCount + "."
+     * @param expectedTagCount The expected number of AprilTags
+     * @param actualTagCount The actual number of AprilTags
+     */
+    public TooManyTagsDetectedException(int expectedTagCount, int actualTagCount)
+    {
+        super("Too many AprilTags (" + actualTagCount + ") were detected. Expected " + expectedTagCount + ".");
+    }
+}
