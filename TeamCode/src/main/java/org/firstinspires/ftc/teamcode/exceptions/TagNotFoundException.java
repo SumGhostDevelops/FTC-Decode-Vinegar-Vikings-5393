9db66bd1041860a549d863d61ed9a42ddd9c7fe7
Caleb C <crd.5681@outlook.com>
2025-10-27 13:20:35 -0500

    update wording

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/exceptions/TagNotFoundException.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/exceptions/TagNotFoundException.java
@@ -3,19 +3,19 @@ package org.firstinspires.ftc.teamcode.exceptions;
 public class TagNotFoundException extends RuntimeException
 {
     /**
-     * "The AprilTag was not detected."
+     * "The AprilTag was not found."
      */
     public TagNotFoundException()
     {
-        super("The AprilTag was not detected.");
+        super("The AprilTag was not found.");
     }
 
     /**
-     * "The AprilTag with ID " + id + " was not detected."
-     * @param id The ID of the AprilTag that was not detected.
+     * "The AprilTag with ID " + id + " was not found."
+     * @param id The ID of the AprilTag that was not found.
      */
     public TagNotFoundException(int id)
     {
-        super("The AprilTag with ID " + id + " was not detected.");
+        super("The AprilTag with ID " + id + " was not found.");
     }
 }
