b8457af6dcfdccdc292fd9853e603d4e94e7f174
Caleb C <crd.5681@outlook.com>
2025-10-31 23:48:20 -0500

    add wheels class for handling the raw power of wheels for manual movement

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/robot/Wheels.java
@@ -0,0 +1,65 @@
+package org.firstinspires.ftc.teamcode.robot;
+
+public class Wheels
+{
+    private double leftFront;
+    private double leftBack;
+    private double rightFront;
+    private double rightBack;
+
+    public Wheels()
+    {
+        this.leftFront = 0;
+        this.leftBack = 0;
+        this.rightFront = 0;
+        this.rightBack = 0;
+    }
+
+    public Wheels(double leftFront, double leftBack, double rightFront, double rightBack)
+    {
+        this.leftFront = leftFront;
+        this.leftBack = leftBack;
+        this.rightFront = rightFront;
+        this.rightBack = rightBack;
+    }
+
+    public double getLeftFrontPower() {
+        return leftFront;
+    }
+
+    public double getLeftBackPower() {
+        return leftBack;
+    }
+
+    public double getRightFrontPower() {
+        return rightFront;
+    }
+
+    public double getRightBackPower() {
+        return rightBack;
+    }
+
+    public void setLeftFrontPower(double leftFront) {
+        this.leftFront = leftFront;
+    }
+
+    public void setLeftBackPower(double leftBack) {
+        this.leftBack = leftBack;
+    }
+
+    public void setRightFrontPower(double rightFront) {
+        this.rightFront = rightFront;
+    }
+
+    public void setRightBackPower(double rightBack) {
+        this.rightBack = rightBack;
+    }
+
+    public void setAllWheelsPower(double power)
+    {
+        leftFront = power;
+        leftBack = power;
+        rightFront = power;
+        rightBack = power;
+    }
+}
