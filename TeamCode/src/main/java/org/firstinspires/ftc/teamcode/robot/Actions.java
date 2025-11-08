2a7b03586cdc1ad59cee12604e4235a4710e83e0
ianw <ianwight123@gmail.com>
2025-11-08 11:58:25 -0600

    Lowered the tolerance

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/robot/Actions.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/robot/Actions.java
@@ -22,7 +22,7 @@ public class Actions
         double kP = 0.05; // Proportional gain
         double error;
         double motorPower;
-        double tolerance = 2.0; // Stop when within 2 degrees
+        double tolerance = 0.5; // Stop when within this many degrees
 
         do {
             // The IMU gives us the current angle of the robot.
