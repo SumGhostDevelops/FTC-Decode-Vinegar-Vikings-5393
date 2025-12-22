971b7ad63c635c64cfb57084eb42e290bf95e48e
Caleb C <crd.5681@outlook.com>
2025-12-22 00:47:00 -0600

    fix

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/experiments/ActuatorTesting.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/experiments/ActuatorTesting.java
@@ -2,18 +2,11 @@ package org.firstinspires.ftc.teamcode.opmodes.experiments;
 
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
-import com.qualcomm.robotcore.hardware.CRServo;
 import com.qualcomm.robotcore.hardware.DcMotorEx;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
-import com.seattlesolvers.solverslib.gamepad.GamepadKeys;
 
 import org.firstinspires.ftc.teamcode.controls.InputHandler;
 import org.firstinspires.ftc.teamcode.definitions.RobotConstants;
-import org.firstinspires.ftc.teamcode.definitions.Team;
-import org.firstinspires.ftc.teamcode.opmodes.teleop.Base;
-
-import java.util.ArrayList;
-import java.util.List;
 
 @TeleOp(name = "Actuator Testing", group = "Experiments")
 public class ActuatorTesting extends LinearOpMode
