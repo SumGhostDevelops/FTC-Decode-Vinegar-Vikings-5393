8a5ca819df1e4d6612734e2c8a897937e33bc4f2
Caleb C <crd.5681@outlook.com>
2026-01-10 11:07:01 -0600

    fixes and changes

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/autonomous/basic/Red.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/autonomous/basic/Red.java
@@ -1,5 +1,5 @@
-
 package org.firstinspires.ftc.teamcode.opmodes.autonomous.basic;
+
 import android.provider.SyncStateContract;
 import com.pedropathing.follower.Follower;
 import com.pedropathing.geometry.BezierLine;
@@ -18,19 +18,17 @@ import org.firstinspires.ftc.teamcode.definitions.RobotHardware;
 import org.firstinspires.ftc.teamcode.definitions.Team;
 import org.firstinspires.ftc.teamcode.definitions.PedroConstants;
 import org.firstinspires.ftc.teamcode.subsystems.Outtake;
-import java.nio.file.Paths;
+
 import java.util.concurrent.TimeUnit;
 
 @Autonomous(name = "RedBasicVikingsAutonomous", group = "Red", preselectTeleOp = "RedVikingsTeleOp")
 public class Red extends Base
 {
-
-
-    private Paths paths;
+    private AutoPaths paths;
     private Timer timer, opModeTimer;
     private Follower follower;
 
-    private Paths.PathState currentPathState;
+    private AutoPaths.PathState currentPathState;
 
     public void runOpMode() throws InterruptedException {
         initAuto();
@@ -52,9 +50,8 @@ public class Red extends Base
                 robotContext.subsystems.intake.stop();
                 Thread.sleep(3000);
             }
-            follower.followPath(paths.ToShoot);
 
-            Timer endDriving = new Timer();
+            follower.followPath(paths.ToShoot);
 
             while (opModeIsActive() && !isStopRequested()) {
 
@@ -67,51 +64,53 @@ public class Red extends Base
             }
         }
     }
+
     public void initAuto() throws InterruptedException {
         follower = PedroConstants.createFollower(hardwareMap);
-        paths = new Paths(follower);
+        paths = new AutoPaths(follower);
+
+        // Try to set follower pose to the path start pose (works for different follower APIs)
+        try {
+            java.lang.reflect.Method m = follower.getClass().getMethod("setPose", com.pedropathing.geometry.Pose.class);
+            m.invoke(follower, paths.startPose);
+        } catch (NoSuchMethodException e) {
+            try {
+                java.lang.reflect.Method m2 = follower.getClass().getMethod("setPoseEstimate", com.pedropathing.geometry.Pose.class);
+                m2.invoke(follower, paths.startPose);
+            } catch (Exception ignored) {}
+        } catch (Exception ignored) {}
+
         timer = new Timer();
         opModeTimer = new Timer();
         opModeTimer.resetTimer();
-      timer.resetTimer();
+        timer.resetTimer();
     }
 
-    public void setPathState(Paths.PathState pathState) {
+    public void setPathState(AutoPaths.PathState pathState) {
         currentPathState = pathState;
         timer.resetTimer();
     }
 
-
-
-    public static class Paths {
-
+    public static class AutoPaths {
 
         public PathChain ToShoot;
-
+        public final Pose startPose;
 
         public enum PathState {
             ToShoot
-
         }
 
-        public Paths(Follower follower) {
-            final Pose startPose = new Pose(63, 8.3);
+        public AutoPaths(Follower follower) {
+            startPose = new Pose(63, 8.3);
             final Pose shootPose = new Pose(57, 44);
-
 
             ToShoot = follower.pathBuilder().addPath(
                             new BezierLine(
                                     startPose,
-
                                     shootPose
                             )
                     ).setTangentHeadingInterpolation()
                     .build();
-
-
         }
-
-
     }
 }
-
