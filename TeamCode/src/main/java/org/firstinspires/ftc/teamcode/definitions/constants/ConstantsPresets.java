724a617285eca53f4f3607756924887433f97213
Caleb C <crd.5681@outlook.com>
2026-02-05 16:28:00 -0600

    update presets

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/definitions/constants/ConstantsPresets.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/definitions/constants/ConstantsPresets.java
@@ -42,8 +42,8 @@ public class ConstantsPresets
                 break;
             case COMPETITION_TESTING:
                 RobotConstants.Outtake.ON_BY_DEFAULT = true;
-                RobotConstants.Outtake.AUTO_DISTANCE_ADJUSMENT = true;
-                RobotConstants.General.REGRESSION_TESTING_MODE = false;
+                RobotConstants.Outtake.AUTO_DISTANCE_ADJUSMENT = false;
+                RobotConstants.General.REGRESSION_TESTING_MODE = true;
                 RobotConstants.Turret.AUTO_AIM_TO_GOAL = false;
                 RobotConstants.Intake.INTAKE_BY_DEFAULT = false;
         }
