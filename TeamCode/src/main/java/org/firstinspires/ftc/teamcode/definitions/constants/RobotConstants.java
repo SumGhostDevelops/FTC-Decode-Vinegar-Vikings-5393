92e4b36d3f1e51792ba38f040bb43b40ae34809e
Caleb C <crd.5681@outlook.com>
2026-02-19 16:06:01 -0600

    update constant

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/definitions/constants/RobotConstants.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/definitions/constants/RobotConstants.java
@@ -106,7 +106,7 @@ public class RobotConstants
 
         public static boolean USE_LUT = false;
 
-        public static Distance OFFSET_FROM_CENTER = new Distance(-2.5, DistanceUnit.INCH);
+        public static Distance OFFSET_FROM_CENTER = new Distance(-4.5, DistanceUnit.INCH);
 
         @IgnoreConfigurable
         public static class Name
@@ -179,7 +179,7 @@ public class RobotConstants
         public static Angle SAFETY_MARGIN = new Angle(10, AngleUnit.DEGREES);
 
         // right hand rule: forward = x, strafe = y
-        public static Distance OFFSET_FROM_CENTER = new Distance(-5, DistanceUnit.INCH);
+        public static Distance OFFSET_FROM_CENTER = new Distance(-4.5, DistanceUnit.INCH);
     }
 
     @Configurable
