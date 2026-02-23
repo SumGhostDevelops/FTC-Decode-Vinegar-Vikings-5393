647ffcea8519e3696419b414745e48cf4bcc4b14
Caleb C <crd.5681@outlook.com>
2026-02-22 20:24:36 -0600

    feat: add decode future pose prediction to MathUtil.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/util/math/MathUtil.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/util/math/MathUtil.java
@@ -8,7 +8,10 @@ import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit;
 import org.firstinspires.ftc.robotcore.external.navigation.UnnormalizedAngleUnit;
 import org.firstinspires.ftc.teamcode.util.measure.angle.generic.Angle;
 import org.firstinspires.ftc.teamcode.util.measure.angle.generic.UnnormalizedAngle;
+import org.firstinspires.ftc.teamcode.util.measure.coordinate.FieldCoordinate;
+import org.firstinspires.ftc.teamcode.util.measure.coordinate.Pose2d;
 import org.firstinspires.ftc.teamcode.util.measure.distance.Distance;
+import org.firstinspires.ftc.teamcode.util.measure.geometry.Vector2d;
 
 public class MathUtil
 {
@@ -20,6 +23,76 @@ public class MathUtil
     public static int clamp(int value, int minValue, int maxValue)
     {
         return Range.clip(value, minValue, maxValue);
+    }
+
+    public static class Decode
+    {
+        /*
+         * futurePose = currentPose // vector (x, y)
+         * repeat 3 times:
+         * distance = ||targetPos - futurePose|| // Euclidean distance
+         * flightTime = LUT(distance) // or physics formula
+         * newFuturePose = currentPose + robotVelocity * flightTime
+         * futurePose = (futurePose + newFuturePose) / 2 // optional averaging
+         *
+         * //all of the following will probably be outside the function, just wanted to
+         * put it somewhere
+         * distance = ||targetPos - futurePose|| // final distance
+         * turretAngle = atan2(target_y - future_y, target_x - future_x)
+         * flywheelRPM = LUT(distance)
+         */
+
+        public static Pose2d getFuturePose(Pose2d currentPose, Vector2d robotVelocity, FieldCoordinate targetPos, InterpLUT flightTimeLUT)
+        {
+            return getFuturePose(currentPose, robotVelocity, targetPos, flightTimeLUT, 3);
+        }
+
+        public static Pose2d getFuturePose(Pose2d currentPose, Vector2d robotVelocity, FieldCoordinate targetPos, InterpLUT flightTimeLUT, int iterations)
+        {
+            return getFuturePose(currentPose, robotVelocity, targetPos, flightTimeLUT, iterations, true);
+        }
+
+        public static Pose2d getFuturePose(Pose2d currentPose, Vector2d robotVelocity, FieldCoordinate targetPos, InterpLUT flightTimeLUT, int iterations, boolean useDampening)
+        {
+            // We track our "guess" for where we will be
+            double futureX = currentPose.coord.x.getInch();
+            double futureY = currentPose.coord.y.getInch();
+
+            for (int i = 0; i < iterations; i++)
+            {
+                // 1. Calculate distance from our LATEST guess to the target
+                double dx = targetPos.x.getInch() - futureX;
+                double dy = targetPos.y.getInch() - futureY;
+                double distance = Math.sqrt(dx * dx + dy * dy);
+
+                // 2. Get time of flight based on that distance
+                double flightTime = flightTimeLUT.get(distance);
+
+                // 3. Calculate the "instant" prediction (starting from currentPose)
+                double instantX = currentPose.coord.x.getInch() + (robotVelocity.x.getInch() * flightTime);
+                double instantY = currentPose.coord.y.getInch() + (robotVelocity.y.getInch() * flightTime);
+
+                // 4. Update the guess
+                if (useDampening)
+                {
+                    futureX = (futureX + instantX) / 2.0;
+                    futureY = (futureY + instantY) / 2.0;
+                }
+                else
+                {
+                    futureX = instantX;
+                    futureY = instantY;
+                }
+            }
+
+            // Convert back to your Distance units (assuming a .fromInch or similar constructor)
+            return new Pose2d(
+                    new Distance(futureX, DistanceUnit.INCH),
+                    new Distance(futureY, DistanceUnit.INCH),
+                    currentPose.heading.angle,
+                    currentPose.coord.coordSys
+            );
+        }
     }
 
     public static class Motor
